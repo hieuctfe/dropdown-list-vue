7457781841fd2234bf23c755f0daabdcda5b5b2a
hieuctfe <hieuctfe@gmail.com>
2021-06-08 16:26:38 +0700

    dropdown first commit

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <Dropdown
+    v-for="(dropdown, index) in dropdowns"
+    :ref="`dropdown-${index}`"
+    @selected="(data) => seleteData(data, index)"
+    :defaultSelected="'ab'"
+    :options="dropdown"
+    :key="index"
+  />
+</template>
+
+<script>
+import Dropdown from "./components/Dropdown.vue";
+
+export default {
+  name: "App",
+  components: {
+    Dropdown,
+  },
+  data() {
+    return {
+      years: [
+        {
+          name: "1996",
+          id: "1996",
+        },
+        {
+          name: "1997",
+          id: "1997",
+        },
+        {
+          name: "1998",
+          id: "1998",
+        },
+        {
+          name: "1999",
+          id: "1999",
+        },
+        {
+          name: "2000",
+          id: "2000",
+        },
+        {
+          name: "2001",
+          id: "2001",
+        },
+        {
+          name: "2002",
+          id: "2002",
+        },
+      ],
+      Make: [
+        {
+          name: "Honda",
+          id: "Honda",
+        },
+        {
+          name: "Toyota",
+          id: "Toyota",
+        },
+        {
+          name: "Yamaha",
+          id: "Yamaha",
+        },
+        {
+          name: "Mecerdes",
+          id: "Mecerdes",
+        },
+        {
+          name: "Porsche",
+          id: "Porsche",
+        },
+        {
+          name: "Audi",
+          id: "Audi",
+        },
+        {
+          name: "Aucura",
+          id: "Aucura",
+        },
+      ],
+    };
+  },
+  computed: {
+    dropdowns() {
+      return [this.years, this.Make];
+    },
+  },
+  methods: {
+    seleteData(data, index) {
+      console.log(data);
+      this.$refs[`dropdown-${index + 1}`] &&
+        this.$refs[`dropdown-${index + 1}`].showOptions();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
