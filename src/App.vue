180c7716264e71e0ebaddc2feb0ee6627e71cd35
hieuctfe <hieuctfe@gmail.com>
2021-06-08 23:58:28 +0700

    change styling

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div class="app">
     <img style="margin-bottom: 20px" :src="'./2.jpg'" />
     <MultipleDropdown />
-    <img style="margin-top: 20px" :src="'./1.jpg'" />
+    <div><button @click="moveToNext">Search Part</button></div>
+    <img style="margin-top: 20px" class="bottom-image" :src="'./1.jpg'" />
   </div>
 </template>
 
@@ -15,12 +16,32 @@ export default {
     MultipleDropdown,
   },
   computed: {},
-  methods: {},
+  methods: {
+    moveToNext() {
+      window.location.href =
+        "https://www.partsgeek.com/ymm/2020/autocar_llc-dt-/xpeditor.html";
+    },
+  },
 };
 </script>
 
 <style scoped>
 .app {
   background: white;
+}
+button {
+  color: #fff;
+  background-color: #ef6f1a;
+  border-color: #ef6f1a;
+  font-weight: 700;
+  font-size: 1.75rem;
+  width: 300px;
+  border-radius: 10px;
+  border: 0;
+  height: 70px;
+  margin-top: 20px;
+}
+button :hover {
+  cursor: pointer;
 }
 </style>
